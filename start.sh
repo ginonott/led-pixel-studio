d24e56277f1395cd677a9378f5facd6b6178bbec
Gino Notto <ginojnotto@gmail.com>
2023-12-11 17:01:24 -0500

    .

--- start.sh
+++ start.sh
@@ -1,27 +1,15 @@
 #!/usr/bin/env bash
-function cleanup {
-    echo "Cleaning up..."
-
-    for pid in $(ps -ef | grep "next" | awk '{print $2}'); do kill -9 $pid; done
-    for pid in $(ps -ef | grep "flask" | awk '{print $2}'); do kill -9 $pid; done
-}
-
-# clean up any previous instances
-cleanup
 
 # start the server first so we can start asap
 cd server
+kill `cat server.pid`
+
 pip install -r requirements.txt
-FLASK_APP=studio.app.py flask run -h 0.0.0.0 --reload &
-API_SERVER=$!
+nohup FLASK_APP=studio.app.py flask run -h 0.0.0.0 --reload &
+echo $! > server.pid
 
 cd ../client
+kill `cat client.pid`
 npm i
-npm run dev &
-CLIENT_SERVER=$!
-
-# kill proceses on exit
-trap "cleanup $CLIENT_SERVER $API_SERVER" EXIT
-
-wait $CLIENT_SERVER $API_SERVER
-
+nohup npm run dev &
+echo $! > client.pid
