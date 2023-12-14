0daccf3c7eef1a698c9e7eab9c9370a5c17f5ba7
Gino Notto <ginojnotto@gmail.com>
2023-12-14 14:48:04 -0500

    .

--- start.sh
+++ start.sh
@@ -11,3 +11,11 @@ cd ../client
 pkill -f "next dev"
 npm i
 nohup npm run dev &
+
+cd ../
+
+# preload the home page
+curl 0.0.0.0:3000
+
+# tail the client and server logs
+tail client/nohup.out server/nohup.out
