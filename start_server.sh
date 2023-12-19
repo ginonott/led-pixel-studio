9e1dd3ea87fa4b707e5b0a0922c9f661d7783197
Gino Notto <ginojnotto@gmail.com>
2023-12-19 15:58:44 -0500

    .

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash 
 
 cd server
-sudo pkill -f "studio.app"
+sudo -E pkill -f "studio.app"
 
-sudo pip install -r requirements.txt
-sudo nohup python -m studio.app &
+sudo -E pip install -r requirements.txt
+sudo -E nohup python -m studio.app &
 tail nohup.out
